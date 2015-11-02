7483cb3aa641884e336bc386697f6e564035f9ef
Nathan Samson <nathan@nathansamson.be>
2015-11-02 23:53:32 +0100

    Improved support for links

--- lib/odf-report/link.rb
+++ lib/odf-report/link.rb
@@ -10,17 +10,20 @@ module ODFReport
     private
 
     def find_link_node(doc)
-      links = doc.xpath(".//text:a[@xlink:href='http://#{@name}/']")
-      if links.empty?
-        links = doc.xpath(".//text:a[@xlink:href='https://#{@name}/']")
-      end
-
+      links = doc.xpath(".//text:a[imatches(@xlink:href, '^http(s)?:\/\/#{@name}\/?$')]", XPathWorkaround.new)
+      links = doc.xpath(".//text:a[imatches(@xlink:href, '^http(s)?:\/\/#{@name}\/?$')]", XPathWorkaround.new)
       unless links.empty?
         return links.first
       end
       nil
     end
 
+
+    class XPathWorkaround
+      def imatches(node_set, regex)
+        node_set.find_all { |node| node.value =~ /#{regex}/i }
+      end
+    end
   end
 
 end
