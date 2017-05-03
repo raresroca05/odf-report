cc87ceb846572844dfea391efebed827ff1af310
christophe <christophe@beeple.eu>
2017-05-03 16:06:14 +0200

    added styling to calendar (so it properly exports to pdf)

--- lib/odf-report/calendar.rb
+++ lib/odf-report/calendar.rb
@@ -12,13 +12,13 @@ module ODFReport
       @locale = opts[:locale] || 'en'
       @template = nil
       @fields = []
-      @collection = opts[:collection] || { Date.new(2017, 6, 6).to_s.to_sym => [{:event_name => 'Team1', :event_location => 'Brussels'},{:event_name => 'Team1', :event_location => 'Brussels'}],
-                                           Date.new(2017, 6, 14).to_s.to_sym => [{:event_name => 'Team2', :event_location => 'Antwerp'}]}
+      @collection = opts[:collection] || []
     end
 
     def replace!(content)
 
       # make the template
+      create_calendar_style content
       define_template content
       # create and save the table, all cells are filled in with the template
       return {} unless @table = find_section_node(content)
@@ -32,27 +32,52 @@ module ODFReport
         node.replace(node.children)
       end
 
-      # now: replace all the tags
-      # problem: shouldn't this be done on a cell per cell basis?
-      # -> immediate replace in generate_cell instead? (probably)
-
     end
 
     private
 
+    def create_calendar_style(doc)
+      style_root = doc.at(".//office:automatic-styles")
+      table_style = Nokogiri::XML::Node.new('style:style', doc)
+      table_style['style:name'] = 'BeepleCalendar'
+      table_style['style:family'] = 'table'
+      table_node = Nokogiri::XML::Node.new('style:table-properties', doc)
+      table_node['style:width'] = '6.6924in'
+      table_node['table:align'] = 'margins'
+      table_style.add_child table_node
+
+      row_style = Nokogiri::XML::Node.new('style:style', doc)
+      row_style['style:name'] = 'BeepleCalendarRow'
+      row_style['style:family'] = 'table-row'
+      row_node = Nokogiri::XML::Node.new('style:table-row-properties', doc)
+      row_node['style:min-row-height'] = '0.8694in'
+      row_style.add_child row_node
+
+      cell_style = Nokogiri::XML::Node.new('style:style', doc)
+      cell_style['style:name'] = 'BeepleCalendarCell'
+      cell_style['style:family'] = 'table-cell'
+      cell_node = Nokogiri::XML::Node.new('style:table-cell-properties', doc)
+      cell_node['fo:border'] = '0.25pt solid #000001'
+      cell_node['fo:padding'] = '0.0375in'
+      cell_style.add_child cell_node
+
+      style_root.add_child table_style
+      style_root.add_child row_style
+      style_root.add_child cell_style
+  end
+
     def generate_calendar
 
       table = Builder::XmlMarkup.new(indent: 2)
-      table.tag!('table:table', 'table:name' => 'CALENDAR', 'table:style-name' => 'TABLE') do |table|
+      table.tag!('table:table', 'table:name' => 'BeepleCalendar', 'table:style-name' => 'BeepleCalendar') do |table|
         # columns
-        7.times do
-          table.tag!('table:table-column', '')
-        end
+
+        table.tag!('table:table-column', 'table:number-columns-repeated' => '7')
 
         # header
         table.tag!('table:table-row') do |row|
           7.times do |idx|
-            row.tag!('table:table-cell', 'office:value-type' => 'string') do |cell|
+            row.tag!('table:table-cell', 'table:style-name' => 'BeepleCalendarCell', 'office:value-type' => 'string') do |cell|
               cell.tag!('text:p', Date::DAYNAMES[(idx + 1) % 7])
             end
           end
@@ -63,9 +88,9 @@ module ODFReport
         current_day = @start_day
         end_day = @end_day
         while current_day != end_day.next
-          table.tag!('table:table-row') do |row|
+          table.tag!('table:table-row', 'table:style-name' => 'BeepleCalendarRow') do |row|
             7.times do
-              row.tag!('table:table-cell', 'office:value-type' => 'string') do |cell|
+              row.tag!('table:table-cell', 'table:style-name' => 'BeepleCalendarCell', 'office:value-type' => 'string') do |cell|
                 cell.tag!('text:p', current_day)
                 items = @collection[current_day.to_s.to_sym]
                 next if items.nil?
@@ -133,27 +158,15 @@ module ODFReport
       possible_start_nodes = doc.xpath(".//*[contains(text(), '#{to_start_placeholder}')]")
       possible_end_nodes = doc.xpath(".//*[contains(text(), '#{to_end_placeholder}')]")
 
-      puts "FOUND START #{possible_start_nodes.length} NODES for CALENDAR"
-      puts "FOUND END #{possible_end_nodes.length} NODES for CALENDAR"
-
       return nil unless possible_start_nodes.length == 1
       return nil unless possible_end_nodes.length == 1
-
-      puts "FOUND possible candidates for CALENDAR"
 
       start_node = possible_start_nodes.first
       end_node = possible_end_nodes.first
 
-      puts "Ancestors for start node #{start_node.ancestors.map(&:name)}"
-      puts "Ancestors for end node #{end_node.ancestors.map(&:name)}"
-
       first_common_ancestor = (start_node.ancestors & end_node.ancestors).first
 
-      puts "FIRST COMMON ANCESTOR is #{first_common_ancestor.namespace.prefix}"
-
       generated_section_node = doc.document.create_element('table:table')
-      # generated_section_node.inner_html = generate_calendar
-      puts "GENERATED SECTION NODE is #{generated_section_node.inspect}"
 
       # Effectively we are going to copy all relevant nodes to a new generated section node
       # This will be done in a few steps
@@ -175,7 +188,6 @@ module ODFReport
                             else
                               start_node.ancestors[common_ancestor_start_index - 1]
                             end
-      puts "PARENTAL START NODE: #{parental_start_node}"
 
       common_ancestor_end_index = end_node.ancestors.index(first_common_ancestor)
       parental_end_node = if common_ancestor_end_index == 0
@@ -184,11 +196,9 @@ module ODFReport
                             end_node.ancestors[common_ancestor_end_index - 1]
                           end
 
-      puts "PARENTAL END NODE: #{parental_end_node}"
 
       current_node = parental_start_node.next
       parental_start_node.after(generated_section_node)
-      puts "Pasted something after parental start and now it's #{parental_start_node}"
 
       generated_section_node.add_child(
           split_node_front(parental_start_node, start_node)
@@ -208,16 +218,6 @@ module ODFReport
       end_node.remove
 
       generated_section_node
-    end
-
-    def get_table_node
-      node = @table.dup
-
-      name = node.get_attribute('text:name').to_s
-      @idx ||=0; @idx +=1
-      node.set_attribute('text:name', "#{name}_#{@idx}")
-
-      node
     end
 
     def split_node_front(node_to_split, _frontier_node)
